9615755f924738251cb0a1f9b67ee5042a81c502
xinruiming <32240724@qq.com>
2017-06-26 14:47:21 +0800

    add comment for api

--- FaceIdentification/src/api/face_rec.cpp
+++ FaceIdentification/src/api/face_rec.cpp
@@ -1,33 +1,3 @@
-/*
-*
-* This file is part of the open-source SeetaFace engine, which includes three modules:
-* SeetaFace Detection, SeetaFace Alignment, and SeetaFace Identification.
-*
-* This file is part of the SeetaFace Identification module, containing codes implementing the
-* face identification method described in the following paper:
-*
-*
-*   VIPLFaceNet: An Open Source Deep Face Recognition SDK,
-*   Xin Liu, Meina Kan, Wanglong Wu, Shiguang Shan, Xilin Chen.
-*   In Frontiers of Computer Science.
-*
-*
-* Copyright (C) 2016, Visual Information Processing and Learning (VIPL) group,
-* Institute of Computing Technology, Chinese Academy of Sciences, Beijing, China.
-*
-* The codes are mainly developed by Jie Zhang(a Ph.D supervised by Prof. Shiguang Shan)
-*
-* As an open-source face recognition engine: you can redistribute SeetaFace source codes
-* and/or modify it under the terms of the BSD 2-Clause License.
-*
-* You should have received a copy of the BSD 2-Clause License along with the software.
-* If not, see < https://opensource.org/licenses/BSD-2-Clause>.
-*
-* Contact Info: you can send an email to SeetaFace@vipl.ict.ac.cn for any problems.
-*
-* Note: the above information must be kept whenever or wherever the codes are used.
-*
-*/
 #include "face_rec.h"
 #include <stdlib.h>
 #include <pthread.h>
@@ -285,7 +255,6 @@ int Face_Rec_Extract(int ChannelID,ImageData img_data_color,ImageData img_data_g
 //  img_data_color: Original Image,
 //  img_data_gray: Gray Image,
 //  callback_function: Callback when complete detect
-
 //Return Value:
 //  0: Noraml, -1: Module Busy, -2: Thread Number exceed the max of thread
 int Face_Rec_Detect(int ChannelID,ImageData img_data_color,ImageData img_data_gray,Face_Rec_Detect_cb_t callback_function)
@@ -312,7 +281,6 @@ int Face_Rec_Detect(int ChannelID,ImageData img_data_color,ImageData img_data_gr
 //Param : 
 //  img1_fea: image 1 feature value
 //  img2_fea: image 2 feature value
-
 //Return Value:
 //  simularity of two faces
 float Face_Rec_Compare(float * img1_fea,float * img2_fea)
