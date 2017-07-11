50521b5508adf5478397960c2f76ae0b4ac3f11e
xinruiming <32240724@qq.com>
2017-07-11 14:55:30 +0800

    sync with windows plus single thread

--- FaceIdentification/src/api/demo.cpp
+++ FaceIdentification/src/api/demo.cpp
@@ -25,8 +25,8 @@ ImageData gallery_dst_data_gray;
 
 static void Face_Rec_Extract_callback1(int state,int FaceNum,float* img_fea);
 static void Face_Rec_Extract_callback2(int state,int FaceNum,float* img_fea);
-static void Face_Rec_Extract_callback3(int state,int FaceNum,std::vector<seeta::FaceInfo> face);
-static void Face_Rec_Extract_callback4(int state,int FaceNum,std::vector<seeta::FaceInfo> face);
+static void Face_Rec_Extract_callback3(int state,int FaceNum,void *face_data);
+static void Face_Rec_Extract_callback4(int state,int FaceNum,void *face_data);
 
 void Face_Rec_Extract_callback1(int state,int FaceNum,float* img_fea)
 {
@@ -50,26 +50,28 @@ void Face_Rec_Extract_callback2(int state,int FaceNum,float* img_fea)
 		std::cout << "No face for Picture 2" <<endl;
 	}
 }
-void Face_Rec_Extract_callback3(int state,int FaceNum,std::vector<seeta::FaceInfo> face)
+void Face_Rec_Extract_callback3(int state,int FaceNum,void *face_data)
 {
+	std::vector<FaceInfo>& result = *(std::vector<FaceInfo>*)face_data;
 	if(state==0)
 	{
-		vector<seeta::FaceInfo>::iterator it=face.begin();
-		std::cout << "picture 1 detect faces:"<<FaceNum<<endl;
+		std::cout << "picture 1 detect faces:"<<FaceNum <<"face roll:"<<result[0].roll<< endl;
 	} else {
 		std::cout << "Face_Rec_Extract_callback3:"<<state <<endl;
 	}
 }
-void Face_Rec_Extract_callback4(int state,int FaceNum,std::vector<seeta::FaceInfo> face)
+
+void Face_Rec_Extract_callback4(int state,int FaceNum,void *face_data)
 {
+	std::vector<FaceInfo>& result = *(std::vector<FaceInfo>*)face_data;
 	if(state==0)
 	{
-		vector<seeta::FaceInfo>::iterator it=face.begin();
-		std::cout << "picture 2 detect faces:"<<FaceNum<<endl;
+		std::cout << "picture 1 detect faces:"<<FaceNum <<"face roll:"<<result[0].roll<< endl;
 	} else {
 		std::cout << "Face_Rec_Extract_callback4:"<<state <<endl;
 	}
 }
+
 int main(int argc, char *argv[]) 
 {
 	char* srcImg=argv[1];
@@ -112,8 +114,8 @@ int main(int argc, char *argv[])
 	
 	Face_Rec_Extract(0,gallery_src_data_color,gallery_src_data_gray,gallery_fea,Face_Rec_Extract_callback1);
 	Face_Rec_Extract(1,gallery_dst_data_color,gallery_dst_data_gray,probe_fea,Face_Rec_Extract_callback2);	
-	Face_Rec_Detect(2,gallery_src_data_color,gallery_src_data_gray,Face_Rec_Extract_callback3);
-	Face_Rec_Detect(3,gallery_dst_data_color,gallery_dst_data_gray,Face_Rec_Extract_callback4);
+	Face_Rec_Detect(2,gallery_src_data_color,gallery_src_data_gray,NULL,Face_Rec_Extract_callback3);
+	Face_Rec_Detect(3,gallery_dst_data_color,gallery_dst_data_gray,NULL,Face_Rec_Extract_callback4);
 
 	while(1)
 	{
@@ -139,6 +141,5 @@ int main(int argc, char *argv[])
 	std::cout << "demo is over, press any key to exit!!!"<<endl;
 	
 	//getch();
-	//only windows
 	return 0;
 }
